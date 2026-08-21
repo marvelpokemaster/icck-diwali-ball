5a669849c81a481fb401eaf5583e93601268988a
marvelpokemaster <marvelpokemaster@gmail.com>
2026-08-21 12:31:26 +0530

    Update diwali page background to Background_hero.png

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,7 +20,7 @@ import { useEffect, useState } from "react";
 import icckLogo from "@/assets/icck-identity/240919_ICCK_horizontal_A_ENG_KOR-color.svg";
 import icckGoldLogoHorizontal from "@/assets/icck-identity/240919_ICCK_horizontal_A_ENG_KOR-gold.svg";
 import icckGoldLogo from "@/assets/icck-identity/240919_ICCK_vertical_ENG-gold.svg";
-import diwaliFlyerBg from "@/assets/diwali-flyer-bg.jpg";
+import diwaliFlyerBg from "@/assets/Background_hero.png";
 import real2025Stage from "@/assets/real-2025-diwali-stage.png";
 import eventPic1 from "@/assets/event-pic-1.png";
 import eventPic2 from "@/assets/event-pic-2.png";
